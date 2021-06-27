e7d1a5b5fdbf934e29116ad64efc134602e9d54f
yang <yang@atguigu.com>
2021-06-27 19:07:49 +0800

    hot commit2

--- src/main/java/com/atguigu/gitWordTest.java
+++ src/main/java/com/atguigu/gitWordTest.java
@@ -1,5 +1,8 @@
 package com.atguigu;
 
 public class gitWordTest {
-
+    public static void main(String[] args) {
+        System.out.println("git test2");
+        System.out.println("git test3");
+    }
 }
